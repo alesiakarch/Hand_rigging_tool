321d361a73fe7135c02ed921235d5b2ad35a3eef
alesiakarch <karchevskaya.alesya@gmail.com>
2024-12-09 20:52:03 +0000

    added plan for hand script

--- hand_script.py
+++ hand_script.py
@@ -4,4 +4,20 @@ import riglib as rl
 sys.path.insert(0, cmds.workspace(expandName = 'scripts'))
 sys.path.insert(0, cmds.workspace(expandName = 'scenes'))
 
+# duplicate rename to set up joing chains
 rl.duplicate_rename_joints()
+
+# parent fk controls
+rl.add_fkFK_cntrls()
+
+# create IK handles for each finger, set prefered angle + basic rotate plane solver (fingers somewhat work but still twist uncontrollably)
+
+# add a polevector to each finger? this is such and overkill but the fingers twist :(
+
+# create IK FK control
+rl.ik_fk_switch()
+
+# hook IK FK together
+rl.leg_blend()
+rl.sdk_ik_fk_visibility()
+# create a wrist twist (adds serveral joints and hooks them up together with the result chain controls)
